083160f7ee0a920d8e80c1e646f3affbb249cd7a
jwats287 <jwats287@users.noreply.github.com>
2018-11-06 08:43:39 -0500

    Styled CSS Land pages

--- client/src/containers/CSSLand/CSS1/CSS1.jsx
+++ client/src/containers/CSSLand/CSS1/CSS1.jsx
@@ -4,9 +4,40 @@ import CSSHeader from '../../../components/CSSHeader/CSSHeader';
 class CSS1 extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="container">
             <CSSHeader/>
-            Page 2
+            <div className="row">
+                <div style={{backgroundColor: 'blue', minHeight: '20vh', width: '66%', display:'inline-block'}} onClick={() => {alert("I'm blue!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>Welcome to React</h1>
+                </div>
+                <div style={{backgroundColor: 'green', minHeight: '20vh', width: '33%', display:'inline-block'}} onClick={() => {alert("I'm green!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>Underworld</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div style={{backgroundColor: 'red', minHeight: '20vh', width: '33%', display:'inline-block'}} onClick={() => {alert("I'm red!")}}>
+                    <h1 style={{color: 'black', textAlign:'center',margin: 0, lineHeight:'20vh'}}>This is</h1>
+                </div>
+                <div style={{backgroundColor: 'yellow', minHeight: '20vh', width: '66%', display:'inline-block'}} onClick={() => {alert("I'm yellow!")}}>
+                    <h1 style={{color: 'black', textAlign:'center',margin: 0, lineHeight:'20vh'}}>the Rainbow Dungeon</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div style={{backgroundColor: 'orange', minHeight: '20vh', width: '66%', display:'inline-block'}} onClick={() => {alert("I'm orange!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>This is a dangerous</h1>
+                </div>
+                <div style={{backgroundColor: 'purple', minHeight: '20vh', width: '33%', display:'inline-block'}} onClick={() => {alert("I'm purple!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>place</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div style={{backgroundColor: 'gray', minHeight: '20vh', width: '33%', display:'inline-block'}} onClick={() => {alert("I'm gray!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>And watch</h1>
+                </div>
+                <div style={{backgroundColor: 'brown', minHeight: '20vh', width: '66%', display:'inline-block'}} onClick={() => {alert("I'm brown!")}}>
+                    <h1 style={{color: 'white', textAlign:'center',margin: 0, lineHeight:'20vh'}}>your style.</h1>
+                </div>
+            </div>
             </div>
         );
     }
--- client/src/containers/CSSLand/CSS2/CSS2.jsx
+++ client/src/containers/CSSLand/CSS2/CSS2.jsx
@@ -4,9 +4,40 @@ import CSSHeader from '../../../components/CSSHeader/CSSHeader';
 class CSS2 extends Component {
     render() {
         return (
+            <div className="container">
+            <CSSHeader/>
             <div className="row">
-                <CSSHeader/>
-                Page 2
+                <div onClick={() => {alert("I'm blue!")}}>
+                    <h1>Welcome to React</h1>
+                </div>
+                <div onClick={() => {alert("I'm green!")}}>
+                    <h1>Underworld</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div onClick={() => {alert("I'm red!")}}>
+                    <h1>This is</h1>
+                </div>
+                <div onClick={() => {alert("I'm yellow!")}}>
+                    <h1>the Rainbow Dungeon</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div onClick={() => {alert("I'm orange!")}}>
+                    <h1>This is a dangerous</h1>
+                </div>
+                <div onClick={() => {alert("I'm purple!")}}>
+                    <h1>place</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div onClick={() => {alert("I'm gray!")}}>
+                    <h1>And watch</h1>
+                </div>
+                <div onClick={() => {alert("I'm brown!")}}>
+                    <h1>your style.</h1>
+                </div>
+            </div>
             </div>
         );
     }
